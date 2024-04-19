f2e925998791979c86332d2a3612b8e897be6325
NathanBaraka <nathanbaraka23@gmail.com>
2024-04-19 12:44:08 +0000

    backend

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    @property
+    def cities(self):
+        """ Getter attribute for cities """
+        from models import storage
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
+
